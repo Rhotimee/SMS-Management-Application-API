3721b772450b84495ff19afdfc9b5d8df0c3b464
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2019-04-28 17:04:08 +0100

    Use dot env

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 
@@ -9,12 +9,12 @@ app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
   res.json({
-    'message': 'Welcome to Timi SMS center'
-  })
+    message: 'Welcome to Timi SMS center',
+  });
 });
 
 
-const { Port } = process.env
+const { Port } = process.env;
 app.listen(Port, () => {
-  console.log(`App running on port ${Port}`)
+  console.log(`App running on port ${Port}`);
 });
