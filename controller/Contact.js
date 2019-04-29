66015fdd06e3bb71b1c87873b76ee91094cd91da
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2019-04-29 01:33:26 +0100

    Catching errors gracefully

--- controller/Contact.js
+++ controller/Contact.js
@@ -4,20 +4,14 @@ class Contacts {
   static async addContact(req, res) {
     const { name, phoneNumber, email } = req.body;
 
-    try {
-      const savedContact = await Contact.create({
-        name, phoneNumber, email,
-      });
+    const savedContact = await Contact.create({
+      name, phoneNumber, email,
+    });
 
-      return res.json({
-        message: 'Contact saved successfully',
-        savedContact,
-      });
-    } catch (error) {
-      return res.json({
-        error,
-      });
-    }
+    return res.status(201).json({
+      message: 'Contact saved successfully',
+      savedContact,
+    });
   }
 }
 
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import Contact from '../../controller/Contact';
+import { catchErrors } from '../../handlers/errorHandlers';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({
-    message: 'hello0',
+    message: 'Welcome',
   });
 });
 
-router.post('/add', Contact.addContact);
+router.post('/add', catchErrors(Contact.addContact));
 
 export default router;
